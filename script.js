75f66abf930262d3cb55d763c15f7e9a6d789f2c
hounch <zenia20043lee@gmail.com>
2023-09-23 11:56:55 +0300

    js modified

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
+var level = 0;
+let gamePattern = [];
+let userClickedButton = [];
+
+let buttonColors = ['red','blue','green','yellow'];
+
+//HARD-TASK: Click 5 buttons if it's 5 level 
+
 $(document).keypress(function() {
-    //random_generator also will call sound and animation
-    random_button_generator()
+    //fix function that it callable only 
+    //when game is not started
+    random_button_generator();
 });
 
 
 $(".btn").click(function() {
     //animation
     var chosenColor = $(this).attr('id'); //yellow
+    userClickedButton.push(chosenColor);
     sounds(chosenColor);
-
     animation(chosenColor);
     random_button_generator();
+    checkAnswer();
+    // console.log(userClickedButton,'user');
+    // console.log(gamePattern,'game');
 });
 
 function sounds(name) {
@@ -25,26 +37,47 @@ function animation(name) {
     }, 300);
 }
 
-function random_button_generator(name) {
-    var randomNumber = Math.floor(Math.random() * 4) + 1;
-    // save current button
-    sounds()
-    animation() 
+function random_button_generator() {
+    level++;
+    $('h1').text('Level: ' + level);
+    var randomNumber = Math.floor(Math.random() * 4);
+    gamePattern.push(buttonColors[randomNumber]);
+    //sound again 
 }
 
 
-function checkAnswer(params) {
+function checkAnswer() {
+    console.log('clicked ' + userClickedButton[userClickedButton.length-1]);
+    console.log('game ',gamePattern[gamePattern.length-1]);
+
+    if (userClickedButton[userClickedButton.length-1] == gamePattern[gamePattern.length-1]) {
+        // next sequence
+        // stop generating and clicking at the same time 
+        console.log('Good');
+
+    } else {
+        //bad sound
+        //show that game is over with ("Game Over, Press Any Key to Restart")
+        //after some time start again 
+        console.log('ne good');
+    }
     
+
+    // switch (name) {
+    //     case name2:
+    //         random_button_generator();
+    //         break;
+    //     default:
+    //         var audio2 = new Audio('sounds/wrong.mp3');
+    //         audio2.play();
+    //         startOver();
+    //         break;
+    // }
 }
 
-function startOver(params) {
-    
+function startOver() {
+    random_button_generator();
 }
 
-function level_keeper(params) {
-    
-}
 
-function next_button(params) {
-    
-}
+
